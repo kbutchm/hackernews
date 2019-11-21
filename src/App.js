079cd52df389ac01f3ce8ab65419ac68de1f4a14
Kevin <kvmo@demant.com>
2019-11-21 08:17:51 -0500

    styling components

--- src/App.js
+++ src/App.js
@@ -20,8 +20,17 @@ const list = [
   }
 ];
 
-// add comment
-// remove const var
+const largeColumn = {
+  width: "40%"
+};
+
+const midColumn = {
+  width: "30%"
+};
+
+const smallColumn = {
+  width: "10%"
+};
 
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -52,10 +61,12 @@ class App extends Component {
   render() {
     const { searchTerm, list } = this.state;
     return (
-      <div className="App">
-        <Search value={searchTerm} onChange={this.onSearchChange}>
-          Search
-        </Search>
+      <div className="page">
+        <div className="interactions">
+          <Search value={searchTerm} onChange={this.onSearchChange}>
+            Search
+          </Search>
+        </div>
         <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
@@ -70,17 +81,22 @@ const Search = ({ value, onChange, children }) => (
 );
 
 const Table = ({ list, pattern, onDismiss }) => (
-  <div>
+  <div className="table">
     {list.filter(isSearched(pattern)).map(item => (
-      <div key={item.objectID}>
-        <span>
+      <div key={item.objectID} className="table-row">
+        <span style={largeColumn}>
           <a href={item.url}>{item.title}</a>
         </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>
-          <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+        <span style={midColumn}>{item.author}</span>
+        <span style={smallColumn}>{item.num_comments}</span>
+        <span style={smallColumn}>{item.points}</span>
+        <span style={smallColumn}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
         </span>
       </div>
     ))}
