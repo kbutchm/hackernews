addcf05df2ee095aaca00c82daa13f5d20cfc60c
Kevin <kvmo@demant.com>
2019-11-21 07:14:17 -0500

    composable components

--- src/App.js
+++ src/App.js
@@ -53,7 +53,9 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <Search value={searchTerm} onChange={this.onSearchChange} />
+        <Search value={searchTerm} onChange={this.onSearchChange}>
+          Search
+        </Search>
         <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
@@ -62,9 +64,10 @@ class App extends Component {
 
 class Search extends Component {
   render() {
-    const { value, onChange } = this.props;
+    const { value, onChange, children } = this.props;
     return (
       <form>
+        {children}
         <input type="text" value={value} onChange={onChange} />
       </form>
     );
